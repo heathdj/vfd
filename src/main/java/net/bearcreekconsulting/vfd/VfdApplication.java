b609faf3c840357fd63e9e076179514ca5886134
David Heath <heathdj@gmail.com>
2019-05-21 15:32:30 -0700

    Initial Project commit

---
+++ src/main/java/net/bearcreekconsulting/vfd/VfdApplication.java
@@ -0,0 +1,13 @@
+package net.bearcreekconsulting.vfd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VfdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VfdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/bearcreekconsulting/vfd/VfdApplicationTests.java
@@ -0,0 +1,16 @@
+package net.bearcreekconsulting.vfd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VfdApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
